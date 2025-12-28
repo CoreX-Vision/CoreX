44fa163e09ec264e6cf31830356f1f73dde4087f
Rimshad <154471865+rimshad1@users.noreply.github.com>
2025-12-28 22:11:13 +0530

    changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://corexvision.com",
+  siteUrl: "https://www.corexvision.com",
   generateRobotsTxt: true, // creates robots.txt automatically
   sitemapSize: 7000,
   changefreq: "weekly",
